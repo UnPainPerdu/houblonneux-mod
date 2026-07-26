2d1b740b5a7b3145b8af22bd26a4b7912bc17629
Ethan Capouillez <ethan.capouillez@hotmail.be>
2026-07-26 18:32:27 +0200

    add model and texture for 2 new bottles

--- src/main/java/com/unpainperdu/houblonneux/datagen/asset/model/ItemModelProvider.java
+++ src/main/java/com/unpainperdu/houblonneux/datagen/asset/model/ItemModelProvider.java
@@ -38,11 +38,11 @@ public class ItemModelProvider
         this.generateItemWithCustomModel(ModItemRegister.EMERALD_CALL_GLASS.get(), "emerald_call_glass");
         this.generateItemWithCustomModel(ModItemRegister.EMERALD_CALL_MUG.get(), "emerald_call_mug");
         //  emerald_call
-        this.generateItemWithCustomModel(ModItemRegister.WORM_HOLE_BOTTLE.get(), "emerald_call_bottle"); //TODO real model
+        this.generateItemWithCustomModel(ModItemRegister.WORM_HOLE_BOTTLE.get(), "worm_hole_bottle");
         this.generateItemWithCustomModel(ModItemRegister.WORM_HOLE_GLASS.get(), "emerald_call_glass"); //TODO real model
         this.generateItemWithCustomModel(ModItemRegister.WORM_HOLE_MUG.get(), "emerald_call_mug"); //TODO real model
         //  emerald_call
-        this.generateItemWithCustomModel(ModItemRegister.GROS_GUEULETON_BOTTLE.get(), "emerald_call_bottle"); //TODO real model
+        this.generateItemWithCustomModel(ModItemRegister.GROS_GUEULETON_BOTTLE.get(), "gros_gueuleton_bottle");
         this.generateItemWithCustomModel(ModItemRegister.GROS_GUEULETON_GLASS.get(), "emerald_call_glass"); //TODO real model
         this.generateItemWithCustomModel(ModItemRegister.GROS_GUEULETON_MUG.get(), "emerald_call_mug"); //TODO real model
     }
