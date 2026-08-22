d73a4d98b71522f8167026a3f7f72976be7c3cfb
Ethan Capouillez <ethan.capouillez@hotmail.be>
2026-08-22 15:40:32 +0200

    add new effect translations

--- src/main/java/com/unpainperdu/houblonneux/datagen/asset/language/CommonLanguageHelper.java
+++ src/main/java/com/unpainperdu/houblonneux/datagen/asset/language/CommonLanguageHelper.java
@@ -1,5 +1,6 @@
 package com.unpainperdu.houblonneux.datagen.asset.language;
 
+import com.unpainperdu.houblonneux.Houblonneux;
 import net.neoforged.neoforge.common.data.LanguageProvider;
 
 public class CommonLanguageHelper
@@ -9,5 +10,10 @@ public class CommonLanguageHelper
     public CommonLanguageHelper(LanguageProvider languageProvider)
     {
         this.languageProvider = languageProvider;
+    }
+
+    public void addConfigCategory(String name, String translation)
+    {
+        languageProvider.add(Houblonneux.MOD_ID + ".configuration." + name, translation);
     }
 }
--- src/main/java/com/unpainperdu/houblonneux/datagen/asset/language/EnglishLanguageProvider.java
+++ src/main/java/com/unpainperdu/houblonneux/datagen/asset/language/EnglishLanguageProvider.java
@@ -33,10 +33,10 @@ public class EnglishLanguageProvider extends LanguageProvider
         CommonLanguageHelper clp = new CommonLanguageHelper(this);
         //neoforge
         //  config
-        this.addConfigCategory(ModServerConfig.BREWING_RECIPE_CATEGORY, "Brewing Recipes");
+        clp.addConfigCategory(ModServerConfig.BREWING_RECIPE_CATEGORY, "Brewing Recipes");
         this.add(ModServerConfig.BREWING_RECIPE_TIME_MULTIPLICATION_FACTOR_TRANSLATION_KEY, "Brewing Recipe Time Multiplication Factor");
         this.add(ModServerConfig.BREWING_RECIPE_TIME_DIVISION_FACTOR_TRANSLATION_KEY, "Brewing Recipe Time Division Factor");
-        this.addConfigCategory(ModServerConfig.BEER_EFFECTS, "Beer Effects");
+        clp.addConfigCategory(ModServerConfig.BEER_EFFECTS, "Beer Effects");
         this.add(ModServerConfig.PAIN_DIEUX_CAN_DESTROY_BLOCKS_TRANSLATION_KEY, "Pain Dieux explosion destroy blocks");
         this.add(ModServerConfig.PAIN_DIEUX_CAN_HURT_OTHER_PLAYER_TRANSLATION_KEY, "Pain Dieux explosion hurt other player");
         //integration
@@ -63,6 +63,10 @@ public class EnglishLanguageProvider extends LanguageProvider
         this.add(ModCreativeTabRegister.MAIN_TAB_TRANSLATION_KEY, "Houblonneux");
         // mob effect
         this.addEffect(ModMobEffectRegister.WORM_HOLE, "Worm Hole");
+        this.addEffect(ModMobEffectRegister.PAIN_DIEUX, "Pain Dieux");
+        this.addEffect(ModMobEffectRegister.REAL_DWARVE, "Real Dwarve");
+        this.addEffect(ModMobEffectRegister.PIED_DE_GEANTS, "Pied de géants");
+        this.addEffect(ModMobEffectRegister.LA_BLANCHE_DE_CHEZ_NOUS, "La Blanche de chez nous");
         // block
         this.add(ModBlockRegister.BEER_DISPENSER.get().asItem(), "Beer Dispenser");
         this.add(ModBlockRegister.COASTER.get().asItem(), "Coaster");
@@ -109,10 +113,5 @@ public class EnglishLanguageProvider extends LanguageProvider
         this.add(ModBlockRegister.BAMBOO_BREWING_BARREL.asItem(), "Bamboo Brewing Barrel");
         this.add(ModBlockRegister.CRIMSON_BREWING_BARREL.asItem(), "Crimson Brewing Barrel");
         this.add(ModBlockRegister.WARPED_BREWING_BARREL.asItem(), "Warped Brewing Barrel");
-    }
-
-    private void addConfigCategory(String name, String translation)
-    {
-        this.add(Houblonneux.MOD_ID + ".configuration." + name, translation);
     }
 }
